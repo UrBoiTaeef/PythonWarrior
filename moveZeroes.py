361ce7445ffccb38aff1a98efbe5a7e96aea6e9f
UrBoiTaeef <taeefhere@gmail.com>
2026-05-17 09:07:44 +0530

    update

---
+++ moveZeroes.py
@@ -0,0 +1,19 @@
+def moveZeroes(arr):
+
+    j = 0
+
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[j] = arr[i]
+            j += 1
+
+    while(j <len(arr)):
+        arr[j] = 0
+        j+=1
+    return arr
+
+
+
+arr = list(map(int,input("Enter the elements: ").split()))
+moveZeroes(arr)
+print(arr)
